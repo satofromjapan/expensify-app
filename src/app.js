594b7b42adee3b8fbc97dd3c0bdf08915dd629ac
Masato Takahashi <masato.takaha@gmail.com>
2017-11-13 13:59:52 -0800

    adding firebase to expensify

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import 'react-dates/lib/css/_datepicker.css';
 import './styles/styles.scss'
+import './firebase/firebase'
+
 
 const store = configureStore();
 const jsx = (
