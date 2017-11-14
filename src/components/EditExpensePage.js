4d412b186b5f1a46450e393240792a238f0617f3
Masato Takahashi <masato.takaha@gmail.com>
2017-11-14 11:59:57 -0800

    setup remove from database

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense, removeExpense } from '../actions/expenses';
+import { editExpense, startRemoveExpense } from '../actions/expenses';
 
 //refactor EditExpensePage to be a class based component
 export class EditExpensePage extends React.Component {
@@ -10,7 +10,7 @@ export class EditExpensePage extends React.Component {
     this.props.history.push('/');
   }
   onClick = () => {
-    this.props.removeExpense({id: this.props.expense.id})
+    this.props.startRemoveExpense({id: this.props.expense.id})
     this.props.history.push('/');
   }
   render() {
@@ -28,7 +28,7 @@ export class EditExpensePage extends React.Component {
 // Set up mapDispatchToProps: editExpense and removeExpense
 const mapDispatchToProps = (dispatch, props) => ({
   editExpense: (id, expense) => dispatch(editExpense(id, expense)),
-  removeExpense: (data) => dispatch(removeExpense(data))
+  startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
 })
 
 const mapStatetoProps = (state, props) => {
