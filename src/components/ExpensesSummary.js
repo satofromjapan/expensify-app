41fb532e6d68f4dc811dc543f18f6ffd034d29e1
Masato Takahashi <masato.takaha@gmail.com>
2018-01-08 11:35:06 -0800

    Added a display for total number of expenses

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,35 +1,47 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import selectExpensesTotal from '../selectors/expenses-total'
-import selectExpenses from '../selectors/expenses'
-import numeral from 'numeral'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { connect } from 'react-redux';
+import numeral from 'numeral';
+import { Link } from 'react-router-dom';
+// Components / Selectors / Action Imports
+import selectExpensesTotal from '../selectors/expenses-total';
+import selectExpenses from '../selectors/expenses';
 
-export const ExpensesSummary = ({expensesCount, expensesTotal}) => (
-  <div className="page-header">
-    <div className="content-container">
-      <h1 className="page-header__title">{
-        expensesCount === 0 ? (
-          <p>No expenses to view</p>
-        ) : (expensesCount === 1 ? (
-          <p>Viewing <span>{expensesCount}</span> expense totalling <span>{numeral(expensesTotal/100).format('$0,0.00')}</span></p>
-        ) : (<p>Viewing <span>{expensesCount}</span> expenses totalling <span>{numeral(expensesTotal/100).format('$0,0.00')}</span></p>)
-        )
-      }
-      </h1>
-      <div className="page-header__actions">
-        <Link className="button-layout" to="/create">Add Expense</Link>
-      </div>
-    </div>
-  </div>
-)
+export const ExpensesSummary = ({
+	displayedExpenses,
+	expensesTotal,
+	totalExpenses
+}) => {
+	const expenseWord = displayedExpenses == 1 ? 'expense' : 'expenses';
+	const allExpenses =
+		displayedExpenses == totalExpenses ? '' : `(out of ${totalExpenses})`;
+	const amount = numeral(totalExpenses / 100).format('$0,0.00');
+	let heading = (
+		<h1 className="page-header__title">
+			Viewing <span>{displayedExpenses}</span> {expenseWord} {allExpenses}{' '}
+			totalling <span>{amount}</span>
+		</h1>
+	);
+	return (
+		<div className="page-header">
+			<div className="content-container">
+				{heading}
+				<div className="page-header__actions">
+					<Link className="button-layout" to="/create">
+						Add Expense
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-const mapStateToProps = (state) => {
-  const visibleExpenses = selectExpenses(state.expenses, state.filters)
-  return {
-    expensesCount: visibleExpenses.length,
-    expensesTotal: selectExpensesTotal(visibleExpenses)
-  }
-}
+const mapStateToProps = state => {
+	const visibleExpenses = selectExpenses(state.expenses, state.filters);
+	return {
+		displayedExpenses: visibleExpenses.length,
+		expensesTotal: selectExpensesTotal(visibleExpenses),
+		totalExpenses: state.expenses.length
+	};
+};
 
-export default connect(mapStateToProps)(ExpensesSummary)
+export default connect(mapStateToProps)(ExpensesSummary);
